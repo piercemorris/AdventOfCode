c1e756904f5b651829dfb65ab6d7528fd3f7269b
Pierce Morris <pierce.morris@bjss.com>
2021-12-02 05:41:18 +0000

    day 2 needs refactor

--- src/2021/days/day2.ts
+++ src/2021/days/day2.ts
@@ -2,9 +2,39 @@ import { Day } from "../../models/types"
 
 export default {
     solvePartOne: (input: string[]): string => {
-        return ''
+        let depth = 0
+        let horizontal = 0
+        
+        for (let i = 0; i < input.length-1; i++) {
+            let section = input[i].split(' ')
+            if (section[0] === 'forward') {
+                horizontal += parseInt(section[1])
+            } else if (section[0] === 'down') {
+                depth += parseInt(section[1])
+            } else if (section[0] === 'up') {
+                depth -= parseInt(section[1])
+            }
+        }
+        console.log(depth, horizontal)
+        return (depth*horizontal).toString()
     },
     solvePartTwo: (input: string[]): string => {
-        return ''
+        let depth = 0
+        let horizontal = 0
+        let aim = 0
+        
+        for (let i = 0; i < input.length-1; i++) {
+            let section = input[i].split(' ')
+            if (section[0] === 'forward') {
+                horizontal += parseInt(section[1])
+                depth += parseInt(section[1]) * aim
+            } else if (section[0] === 'down') {
+                aim += parseInt(section[1])
+            } else if (section[0] === 'up') {
+                aim -= parseInt(section[1])
+            }
+        }
+        console.log(depth, horizontal)
+        return (depth*horizontal).toString()
     }
 } as Day
