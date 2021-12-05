c9db2c5538b58dcd205b577741b66e4d9faaf73d
Pierce Morris <pierce.morris@bjss.com>
2021-12-05 15:47:57 +0000

    solve day 4 part 1

--- src/2021/days/day4.ts
+++ src/2021/days/day4.ts
@@ -1,22 +1,72 @@
 import { Day } from "../../models/types"
 
 type Bingo = {
-    drawnNumbers: string[],
-    boards: string[][][]
+    numbersToDraw: number[],
+    boards: number[][][]
 }
 
 export default {
     solvePartOne: (input: string[]): string => {
-        const { drawnNumbers, boards } = decodeInput(input)
-        return ''
+        const { numbersToDraw, boards } = formatInput(input)
+        let drawnNumbers = []
+        let score = 0
+
+        for(let i = 0; i < numbersToDraw.length; i++) {
+            drawnNumbers.push(numbersToDraw[i])
+            score = checkBoardsForWin(boards, drawnNumbers)
+            if (score > 0) break
+        }
+
+        return score.toString()
     },
     solvePartTwo: (input: string[]): string => {
         return ''
     }
 } as Day
 
-function decodeInput(input: string[]): Bingo {
-    const drawnNumbers = input[0].split(',')
+function calculateWinningScore(board: number[][], drawnNumbers: number[]): number {
+    let tally = 0
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            if(!drawnNumbers.includes(board[y][x])) {
+                tally += board[y][x]
+            }
+        }
+    }
+
+    return tally*drawnNumbers[drawnNumbers.length-1]
+}
+
+function checkBoardsForWin(boards: number[][][], drawnNumbers: number[]): number {
+    for(let boardIndex = 0; boardIndex < boards.length; boardIndex++) {
+        for (var i = 0; i < boards[0].length; i++) {
+            const board = boards[boardIndex]
+            const row = board[i]
+            const column = [board[0][i], board[1][i], board[2][i], board[3][i], board[4][i]] // hardcoded for 5 (fix)
+            const isRow = isDrawnMatches(drawnNumbers, row)
+            const isColumn =  isDrawnMatches(drawnNumbers, column)
+
+            if (isRow) {
+                return calculateWinningScore(board, drawnNumbers)
+            }
+
+            if (isColumn) {
+                console.log(board, drawnNumbers, column)
+                return calculateWinningScore(board, drawnNumbers)
+            }
+        }
+    }
+    return 0
+}
+
+
+
+function isDrawnMatches(drawnNumbers: number[], targets: number[]) {
+    return targets.every(target => drawnNumbers.includes(target))
+}
+
+function formatInput(input: string[]): Bingo {
+    const numbersToDraw = input[0].split(',').map(val => parseInt(val))
     const boards = []
     let tempBoard = []
 
@@ -27,12 +77,8 @@ function decodeInput(input: string[]): Bingo {
             continue
         }
 
-        tempBoard.push(input[i].split(' ').filter(val => val !== ''))
+        tempBoard.push(input[i].split(' ').filter(val => val !== '').map(val => parseInt(val)))
     }
 
-    return { drawnNumbers, boards }
-}
-
-function checkWinningBoard() {
-
+    return { numbersToDraw, boards }
 }
