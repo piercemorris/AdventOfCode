1a9af715daf9edc587cd1051253134c44aaac354
piercejmorris <pierce.morris1998@gmail.com>
2021-12-08 14:08:43 +0000

    almost solved day 7 part 2

--- src/2021/days/day7.ts
+++ src/2021/days/day7.ts
@@ -1,9 +1,4 @@
 import { Day } from "../../models/types"
-import { typedKeys } from "../../utils/util"
-
-interface NumberObject {
-    [key: number]: number
-}
 
 export default {
     solvePartOne: (input: string[]): string => {
@@ -12,12 +7,27 @@ export default {
         return calculateFuelCost(positions, medianCrabPosition).toString()
     },
     solvePartTwo: (input: string[]): string => {
-        return ''
+        const positions = formatInput(input)
+        const averageCrabPosition = calculateAverageCrabPosition(positions)
+        return calculateIncreasingFuelCost(positions, averageCrabPosition).toString()
     }
 } as Day
 
 function calculateMedianCrabPosition(positions: number[]): number {
     return positions.sort((a, b) => a - b)[positions.length/2]
+}
+
+function calculateAverageCrabPosition(positions: number[]): number {
+    return Math.round(positions.reduce((prev, curr) => prev + curr)/positions.length)
+}
+
+function calculateIncreasingFuelCost(positions: number[], position: number): number {
+    let fuel = 0
+    for (var i = 0; i < positions.length; i++) {
+        const difference = Math.abs(positions[i] - position)
+        fuel += (difference*(difference+1))/2
+    }
+    return fuel
 }
 
 function calculateFuelCost(positions: number[], position: number): number {
