92ed105f0693e68bcd395028d6c6eecf70bf1176
Pierce Morris <pierce.morris1998@gmail.com>
2022-12-04 06:41:23 +0000

    solve for day 4

--- src/2022/day4.ts
+++ src/2022/day4.ts
@@ -1,9 +1,37 @@
 
 export default {
   solvePartOne: (input: string[]) => {
-    return ''
+    let completeOverlaps = 0
+    for (var i = 0; i < input.length; i++) {
+      const sections = input[i].split(',')
+      const firstElve = sections[0].split('-')
+      const secondElve = sections[1].split('-')
+
+      if (parseInt(firstElve[0]) <= parseInt(secondElve[0]) && 
+        parseInt(firstElve[1]) >= parseInt(secondElve[1])) {
+          completeOverlaps += 1
+      } else if (parseInt(firstElve[0]) >= parseInt(secondElve[0]) && 
+        parseInt(firstElve[1]) <= parseInt(secondElve[1])) {
+          completeOverlaps += 1
+      }
+    }
+    return completeOverlaps
   },
   solvePartTwo: (input: string[]) => {
-    return ''
+    let completeOverlaps = 0
+    for (var i = 0; i < input.length; i++) {
+      const sections = input[i].split(',')
+      const firstElve = sections[0].split('-')
+      const secondElve = sections[1].split('-')
+
+      if (parseInt(firstElve[0]) <= parseInt(secondElve[0]) && 
+        parseInt(firstElve[1]) >= parseInt(secondElve[0])) {
+          completeOverlaps += 1
+      } else if (parseInt(firstElve[0]) >= parseInt(secondElve[0]) && 
+        parseInt(firstElve[0]) <= parseInt(secondElve[1])) {
+          completeOverlaps += 1
+      }
+    }
+    return completeOverlaps
   }
 }
