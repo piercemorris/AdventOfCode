5ab91aab76f0e2beae942d5103ad9327343860ce
Pierce Morris <pierce.morris1998@gmail.com>
2022-12-06 10:06:49 +0000

    solve day 6

--- src/2022/day6.ts
+++ src/2022/day6.ts
@@ -1,9 +1,42 @@
 
+function uniqueChars(code: string[]) {
+  return !code.some((v, i) => code.indexOf(v) < i);
+}
+
 export default {
   solvePartOne: (input: string[]) => {
-    return ''
+    const chars = input[0].split('')
+    let index = 0
+    let string = []
+
+    for (var i = 0; i < chars.length; i++) {
+      string.push(chars[i])
+      if (string.length === 4) {
+        if (uniqueChars(string)) {
+          index = i + 1
+          break
+        }
+        string.shift()
+      }
+    }
+
+    return index
   },
   solvePartTwo: (input: string[]) => {
-    return ''
+    const chars = input[0].split('')
+    let index = 0
+    let string = []
+
+    for (var i = 0; i < chars.length; i++) {
+      string.push(chars[i])
+      if (string.length === 14) {
+        if (uniqueChars(string)) {
+          index = i + 1
+          break
+        }
+        string.shift()
+      }
+    }
+    return index
   }
 }
