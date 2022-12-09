f450286502903bc9055a9ad4acddb9c3398c92f2
Pierce Morris <pierce.morris1998@gmail.com>
2022-12-09 09:12:00 +0000

    solve day 8

--- src/2022/day8.ts
+++ src/2022/day8.ts
@@ -1,9 +1,153 @@
+const testInput = [
+  '30373',
+  '25512',
+  '65332',
+  '33549',
+  '35390'
+]
 
 export default {
   solvePartOne: (input: string[]) => {
-    return ''
+    const treeGrid = input
+    let isVisibleLineL: boolean[] = []
+    let isVisibleLineR: boolean[] = []
+    let isVisibleLineT: boolean[] = []
+    let isVisibleLineB: boolean[] = []
+    
+    const isVisibleGridL = []
+    const isVisibleGridR = []
+    const isVisibleGridT = []
+    const isVisibleGridB = []
+
+    let visibleTrees = 0
+
+    for (var y = 0; y < treeGrid.length; y++) {
+      const line = treeGrid[y].split('')
+      isVisibleLineL = []
+      isVisibleLineR = []
+      isVisibleLineT = []
+      isVisibleLineB = []
+      let tallestTreeR = -1
+      let tallestTreeL = -1
+      let tallestTreeT = -1
+      let tallestTreeB = -1
+
+      for (var x = 0; x < line.length; x++) {
+        const currentL = parseInt(line[x])
+        const currentR = parseInt(line[(line.length-1)-x])
+        const currentT = parseInt(treeGrid[x][y])
+        const currentB = parseInt(treeGrid[(line.length-1)-x][y])
+
+        if (currentL > tallestTreeL) {
+          tallestTreeL = currentL
+          isVisibleLineL.push(true)
+        } else {
+          isVisibleLineL.push(false)
+        }
+
+        if (currentR > tallestTreeR) {
+          tallestTreeR = currentR
+          isVisibleLineR.push(true)
+        } else {
+          isVisibleLineR.push(false)
+        }
+
+        if (currentT > tallestTreeT) {
+          tallestTreeT = currentT
+          isVisibleLineT.push(true)
+        } else {
+          isVisibleLineT.push(false)
+        }
+        
+        if (currentB > tallestTreeB) {
+          tallestTreeB = currentB
+          isVisibleLineB.push(true)
+        } else {
+          isVisibleLineB.push(false)
+        }
+      }
+      isVisibleGridL.push(isVisibleLineL)
+      isVisibleGridR.push(isVisibleLineR.reverse())
+      isVisibleGridT.push(isVisibleLineT)
+      isVisibleGridB.push(isVisibleLineB.reverse())
+    }
+
+    for (var y = 0; y < treeGrid.length; y++) {
+      for (var x = 0; x < treeGrid[0].length; x++) {
+        if (isVisibleGridL[y][x] || isVisibleGridR[y][x] || isVisibleGridT[x][y] || isVisibleGridB[x][y]) {
+          visibleTrees += 1
+        }
+      }
+    }
+    return visibleTrees
   },
   solvePartTwo: (input: string[]) => {
-    return ''
+    const treeGrid = input
+    let highestScenicScore = 0
+
+
+    const scenicScores: number[][] = []
+
+    for (var row = 0; row < treeGrid[0].length; row++) {
+      const currentRow = treeGrid[row].split('')
+
+      const rowScenicScores: number[] = []
+
+
+      for (var col = 0; col < currentRow.length; col++) {
+        const currentTreeHeight = currentRow[col]
+        // console.log(`new coord ${row},${col}, ${currentRow[col]}`)
+        let scoreL = 0
+        let scoreR = 0
+        let scoreT = 0
+        let scoreB = 0
+        
+        // left
+        for (var left = col-1; left >= 0; left--) {
+          scoreL++
+          if (currentRow[left] >= currentTreeHeight) break
+        }
+
+        console.log(scoreL);
+        
+
+        // right
+        for (var right = col+1; right < treeGrid.length; right++) {
+          scoreR++
+          if (currentRow[right] >= currentTreeHeight) break
+        }
+        console.log(scoreR);
+
+        // top
+        for (var top = row-1; top >= 0; top--) {
+          scoreT++
+          if (treeGrid[top].split('')[col] >= currentTreeHeight) break
+        }
+        console.log(scoreT);
+
+        // bottom
+        for (var bot = row+1; bot < treeGrid.length; bot++) {
+          scoreB++
+          if (treeGrid[bot].split('')[col] >= currentTreeHeight) break
+        }
+
+        console.log(scoreB);
+
+
+        let total = scoreL * scoreR * scoreT * scoreB
+
+        rowScenicScores.push(total)
+
+        // console.log(total)
+        highestScenicScore = total > highestScenicScore ? total : highestScenicScore
+      }
+
+      scenicScores.push(rowScenicScores)
+    } 
+
+    console.log(scenicScores.map(row => row.map(ss => String(ss)).join(" ")).join("\n\n"));
+    
+
+    return highestScenicScore
   }
 }
